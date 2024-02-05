444dcddc4a0df575c53559d6087ebc5d24c5864f
Simon Stranks <sstranks87@gmail.com>
2024-02-05 14:39:48 +0000

    chore(github ci): linting

--- .github/linters/stylelint.config.cjs
+++ .github/linters/stylelint.config.cjs
@@ -2,4 +2,8 @@ require('@packages/stylelint-config');
 
 module.exports = {
   extends: '@packages/stylelint-config',
+  rules: {
+    'at-rule-no-unknown': off,
+    'comment-empty-line-before': off,
+  },
 };
