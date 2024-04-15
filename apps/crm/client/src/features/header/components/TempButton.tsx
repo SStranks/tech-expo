9cc82afe638d4b3c98521744fb9d51f8d2412f4a
Simon Stranks <sstranks87@gmail.com>
2024-04-16 00:07:15 +0100

    fix(styling): input focus; border color now info color, even when required flag is true

--- apps/crm/client/src/features/header/components/TempButton.tsx
+++ apps/crm/client/src/features/header/components/TempButton.tsx
@@ -2,17 +2,19 @@ import FormModal from '#Components/modal/FormModal';
 import { IconEye } from '#Svg/icons';
 import { useState } from 'react';
 
-// let listItems = [
-//   { name: 'Aerospace' },
-//   { name: 'Mechanical' },
-//   { name: 'Civil' },
-//   { name: 'Biomedical' },
-//   { name: 'Nuclear' },
-//   { name: 'Industrial' },
-//   { name: 'Chemical' },
-//   { name: 'Agricultural' },
-//   { name: 'Electrical' },
-// ];
+let listItems = [
+  { id: 'a', name: 'Aerospace' },
+  { id: 'b', name: 'Mechanical' },
+  { id: 'c', name: 'Civil' },
+  { id: 'd', name: 'Biomedical' },
+  { id: 'e', name: 'Nuclear' },
+  { id: 'f', name: 'Industrial' },
+  { id: 'g', name: 'Chemical' },
+  { id: 'h', name: 'Agricultural' },
+  { id: 'i', name: 'Electrical' },
+];
+
+console.log(listItems);
 
 function TempButton(): JSX.Element {
   const [portalActive, setPortalActive] = useState<boolean>(true);
@@ -47,10 +49,10 @@ function TempButton(): JSX.Element {
         modalTitle={'Form Modal'}
         portalActive={portalActive}
         setPortalActive={setPortalActive}>
-        <FormModal.Input name="input" type="text" label="Piss Stop" rules={{ required: true }} />
-        <FormModal.TextArea name="textarea" label="Piss Stop" rules={{ required: true }} />
-        {/* <FormModal.Select name="inputSelect" label="Piss Stop" items={listItems} rules={{ required: true }} /> */}
-        {/* <FormModal.Combo name="inputCombo" label="Piss Stop" items={listItems} rules={{ required: true }} /> */}
+        <FormModal.Input name="input" type="text" label="Piss Stop1" rules={{ required: true }} />
+        {/* <FormModal.TextArea name="textarea" label="Piss Stop2" rules={{ required: true }} /> */}
+        <FormModal.Select name="inputSelect" label="Piss Stop3" items={listItems} rules={{ required: true }} />
+        <FormModal.Combo name="inputCombo" label="Piss Stop4" items={listItems} rules={{ required: true }} />
         {/* <FormModal.Number
           name="inputNumber"
           label="Piss Stop"
