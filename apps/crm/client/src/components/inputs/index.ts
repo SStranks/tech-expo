3c2523bf1c69979eb35ac01e7d7d496c8fed1f04
Simon Stranks <sstranks87@gmail.com>
2024-04-26 23:45:00 +0100

    chore(crm client): new icons. file deletions

--- apps/crm/client/src/components/inputs/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as InputCombo } from './combo/InputCombo';
-export { default as InputDatePicker } from './date-picker/InputDatePicker';
-export { default as InputNumber } from './number/InputNumber';
-export { default as InputSelect } from './select/InputSelect';
-export { default as InputTimeField } from './time-field/InputTimeField';
-export { default as InputTagGroup } from './tagGroup/InputTagGroup';
-export { default as InputComboTag } from './comboTag/InputComboTag';
