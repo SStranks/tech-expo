fb59229c9a42e1c42ed1545a875878f1aa000a24
Simon Stranks <sstranks87@gmail.com>
2024-07-22 17:03:48 +0100

    chore(crm client): styled company info table

--- apps/crm/client/src/pages/settings/SettingsPage.tsx
+++ apps/crm/client/src/pages/settings/SettingsPage.tsx
@@ -1,6 +1,10 @@
+import { IconCity, IconEmail, IconGlobal, IconPhone } from '#Components/svg';
 import { TableSettingsContacts } from '#Components/tanstack-table/tables';
-import { tableDataSettingsContacts } from '#Data/MockData';
+import { tableDataSettingsContacts, tableDataSettingsCompanyInfo } from '#Data/MockData';
 import styles from './_SettingsPage.module.scss';
+
+// TEMP DEV: // NOTE:  Make dynamic
+const COMPANY_INFO = tableDataSettingsCompanyInfo;
 
 function SettingsPage(): JSX.Element {
   return (
@@ -11,21 +15,47 @@ function SettingsPage(): JSX.Element {
         <table className={styles.table}>
           <thead className={styles.thead}>
             <tr>
-              <th className={styles.th}>Company Info</th>
+              <th className={styles.th}>
+                <span>Company Info</span>
+              </th>
             </tr>
           </thead>
           <tbody className={styles.tbody}>
             <tr className={styles.tbody__tr}>
-              <td className={styles.td}>Address</td>
+              <td className={styles.td}>
+                <div className={styles.companyInfo}>
+                  <IconCity svgClass={styles.companyInfo__icon} />
+                  <span>Address</span>
+                  <p>{COMPANY_INFO[0].address}</p>
+                </div>
+              </td>
             </tr>
             <tr className={styles.tbody__tr}>
-              <td className={styles.td}>Phone</td>
+              <td className={styles.td}>
+                <div className={styles.companyInfo}>
+                  <IconPhone svgClass={styles.companyInfo__icon} />
+                  <span>Phone</span>
+                  <p>{COMPANY_INFO[0].phone}</p>
+                </div>
+              </td>
             </tr>
             <tr className={styles.tbody__tr}>
-              <td className={styles.td}>Email</td>
+              <td className={styles.td}>
+                <div className={styles.companyInfo}>
+                  <IconEmail svgClass={styles.companyInfo__icon} />
+                  <span>Email</span>
+                  <p>{COMPANY_INFO[0].email}</p>
+                </div>
+              </td>
             </tr>
             <tr className={styles.tbody__tr}>
-              <td className={styles.td}>Website</td>
+              <td className={styles.td}>
+                <div className={styles.companyInfo}>
+                  <IconGlobal svgClass={styles.companyInfo__icon} />
+                  <span>Website</span>
+                  <p>{COMPANY_INFO[0].website}</p>
+                </div>
+              </td>
             </tr>
             {/* Empty <tr>; prevents rows from expanding to fill table when total rows height is less than the table height */}
             <tr />
