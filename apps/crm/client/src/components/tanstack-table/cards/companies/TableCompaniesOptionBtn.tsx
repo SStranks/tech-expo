63cb15a85ac57190a0170c9dc8950e93f6d343fb
Simon Stranks <sstranks87@gmail.com>
2024-08-11 12:30:29 +0100

    chore(crm client): extracted function into private scraps; aria modal bug fixed?

--- apps/crm/client/src/components/tanstack-table/cards/companies/TableCompaniesOptionBtn.tsx
+++ apps/crm/client/src/components/tanstack-table/cards/companies/TableCompaniesOptionBtn.tsx
@@ -5,21 +5,6 @@ import { useNavigate } from 'react-router-dom';
 import { IconDelete, IconEye, IconMenuDots } from '#Components/svg';
 import styles from './_TableCompaniesOptionBtn.module.scss';
 
-// NOTE:  // HACK:  Need to extract this functionality; create wrapper components for aria-components that utilize modal functionality (and set padding-right on <html>).
-// NOTE:  Is this even necessary now? No scrollbar seems to be appeared anymore?
-// const temp = (open: boolean) => {
-//   const scrollbarTrackBackground = getComputedStyle(document.querySelector('body')!).getPropertyValue(
-//     '--thm-background-default'
-//   );
-//   if (open) {
-//     document.documentElement.style.backgroundColor = scrollbarTrackBackground;
-//   } else {
-//     document.documentElement.style.removeProperty('background-color');
-//   }
-// };
-
-// console.log(temp);
-
 interface IProps {
   rowOriginal: CoreRow<ITableDataCompanies>['original'];
 }
@@ -29,7 +14,6 @@ function TableCompaniesOptionBtn(props: IProps): JSX.Element {
   const navigate = useNavigate();
 
   return (
-    // <MenuTrigger onOpenChange={(open) => temp(open)}>
     <MenuTrigger>
       <Button className={styles.companiesOptionBtn} aria-label="Companies Option Menu">
         <IconMenuDots svgClass={styles.companiesOptionBtn__svg} />
