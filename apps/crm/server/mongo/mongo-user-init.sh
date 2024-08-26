6a4a90fe6109c041725ce0088a56417936f578cf
Simon Stranks <sstranks87@gmail.com>
2024-08-26 22:51:31 +0100

    chore(crm server): mongodb init scripts

---
+++ apps/crm/server/mongo/mongo-user-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script to generate a new user on a new mongodb instance
+
+echo "*** Preparing MongoDB User Configuration ***"
+sleep 3
+echo "*** Initializing MongoDB User Configuration ***"
+mongosh --username $MONGODB_INITDB_ROOT_USERNAME --password $MONGODB_INITDB_ROOT_PASSWORD << EOF
+use admin
+db.createUser({ user: $MONGODB_USER, pwd: $MONGODB_PASSWORD, roles: [{ role: 'readWrite', db: $MONGODB_DATABASE }] })
+quit()
+EOF
+
+echo "*** Completed MongoDB User Configuration ***"
+exit 0
