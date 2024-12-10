1f83977c4d1d056d65f3244c5e0b2107fa730f3e
Simon Stranks <sstranks87@gmail.com>
2024-12-10 18:31:38 +0000

    chore(tech expo): removed temp folder

--- apps/temp/frontend/cypress/e2e/testApp.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('The Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('/');
-    cy.get('div').contains('This is a test app');
-  });
-});
--- apps/temp/frontend/src/components/App.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './_App.module.scss';
-
-function App(): JSX.Element {
-  return <div className={styles.container}>This is a test app</div>;
-}
-
-export default App;
