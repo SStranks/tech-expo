af0b17b6ae5fe609cfa89bce946a7017fa04bb1a
Simon Stranks <sstranks87@gmail.com>
2024-12-10 16:49:36 +0000

    chore(crm client): added global error handling block

--- apps/crm/client/src/index.tsx
+++ apps/crm/client/src/index.tsx
@@ -1,12 +1,30 @@
+/* eslint-disable perfectionist/sort-imports */
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from '@Components/App';
 import ReduxStore from '@Redux/store';
+import App from '@Components/App';
 
 import '@Sass/global-imports.scss';
+import { authInitialize } from '@Redux/reducers/authSlice';
+
+ReduxStore.dispatch(authInitialize());
+
+// TODO:  Connect global error to Rollbar
+// eslint-disable-next-line unicorn/prefer-add-event-listener
+globalThis.onerror = function (message, source, lineno, colno, error) {
+  // Log the error details or send them to a logging service
+  console.error('Error:', message);
+  console.error('Source:', source);
+  console.error('Line Number:', lineno);
+  console.error('Column Number:', colno);
+  console.error('Error Object:', error);
+
+  // Return true to prevent the default browser error handling
+  return true;
+};
 
 const container = document.querySelector('#root');
 const root = createRoot(container!);
