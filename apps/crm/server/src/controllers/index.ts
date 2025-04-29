c210cd6f3315eac91c0772e8563a6e095db1b4b4
Simon Stranks <sstranks87@gmail.com>
2025-04-29 22:58:00 +0100

    chore(crm server): private testing route/controller

--- apps/crm/server/src/controllers/index.ts
+++ apps/crm/server/src/controllers/index.ts
@@ -1,1 +1,2 @@
 export { default as authController } from './authController.js';
+export { default as testController } from './private.testController.js';
--- apps/crm/server/src/routes/index.ts
+++ apps/crm/server/src/routes/index.ts
@@ -1,1 +1,2 @@
+export { default as testRouter } from './private.testRoutes.js';
 export { default as userRouter } from './userRoutes.js';
