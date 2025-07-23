2e5cf3ac2a2d3f6a52dc9fd5992cdf7799c9bfd3
Simon Stranks <sstranks87@gmail.com>
2025-07-23 11:41:18 +0100

    chore(tech-expo): megalinter configs

--- packages/megalinter-config/.secretlintrc.js
+++ packages/megalinter-config/.secretlintrc.js
@@ -1,16 +1,14 @@
 // NOTE:  MegaLinter v8 only uses '@secretlint/secretlint-rule-preset-recommend'.
-module.exports = {
-  rules: [
-    {
-      id: '@secretlint/secretlint-rule-preset-recommend',
-      rules: [
-        {
-          id: '@secretlint/secretlint-rule-npm',
-          options: {
-            allows: [String.raw`/npm_package_config_[a-zA-Z]+/g`],
-          },
+export const rules = [
+  {
+    id: '@secretlint/secretlint-rule-preset-recommend',
+    rules: [
+      {
+        id: '@secretlint/secretlint-rule-npm',
+        options: {
+          allows: [String.raw`/npm_package_config_[a-zA-Z]+/g`],
         },
-      ],
-    },
-  ],
-};
+      },
+    ],
+  },
+];
