1af795dab5d2ed4e0cccd13a34147d8e74cac905
Simon Stranks <sstranks87@gmail.com>
2025-10-23 00:19:23 +0100

    chore(crm client): amending cypress configuration

--- apps/crm/client/cypress.config.ts
+++ apps/crm/client/cypress.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'cypress';
 
+import path from 'node:path';
+import url from 'node:url';
+
 import WebpackCypressConfig from './cypress/webpack.cypress.js';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 export default defineConfig({
   component: {
+    indexHtmlFile: path.resolve(__dirname, './cypress/support/componentIndex.html'),
     devServer: {
       bundler: 'webpack',
       framework: 'react',
