fa986272f7fb63e068789ce8b10657e1aae28bdd
Simon Stranks <sstranks87@gmail.com>
2025-11-27 14:16:41 +0000

    refactor(crm client): inputux has disabled functionality; fixed validity/dirty state

--- apps/crm/client/src/components/react-hook-form/InputUx.tsx
+++ apps/crm/client/src/components/react-hook-form/InputUx.tsx
@@ -11,17 +11,18 @@ interface IProps extends HTMLAttributes<HTMLDivElement> {
   id: string;
   name: string;
   defaultValue?: string | number;
+  disabled: boolean;
   rules: TValidationRules | undefined;
 }
 
-// Wrapper: UX presentation for state of input; valid, invalid, focused, etc
+// Wrapper: UX presentation for state of input; valid, invalid, focused, disabled, etc
 function InputUx(props: PropsWithChildren<IProps>): React.JSX.Element {
-  const { children, defaultValue, id, label, name, rules, ...rest } = props;
+  const { children, defaultValue, disabled, id, label, name, rules, ...rest } = props;
   const { control } = useFormContext();
-  const { errors, isDirty, isSubmitted } = useFormState({ name, control });
+  const { errors, isDirty: dirty, isSubmitted, isValid } = useFormState({ name, control });
 
   const error = errors[name];
-  const inputValidated = !error && (defaultValue || isDirty);
+  const isDirty = !!defaultValue || dirty;
   const showErrorState = error && isSubmitted;
   const inputRequired = rules?.required && !isDirty && isSubmitted;
 
@@ -30,10 +31,12 @@ function InputUx(props: PropsWithChildren<IProps>): React.JSX.Element {
       {...rest}
       className={clsx(
         `${styles.inputUX}`,
-        `${inputValidated ? styles.success : ''}`,
+        `${isValid ? styles.success : ''}`,
         `${inputRequired ? styles.inputUX__required : ''}`,
         `${showErrorState ? styles.error : ''}`
-      )}>
+      )}
+      aria-disabled={disabled}
+      inert={disabled}>
       {children}
       <label htmlFor={id} className={styles.inputUX__label} data-is-dirty={isDirty}>
         {label}
