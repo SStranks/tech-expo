1d12d3891a0ded6cbf68a616910f8bfd9f830370
Simon Stranks <sstranks87@gmail.com>
2025-12-22 17:50:10 +0000

    chore(crm client): modified global screen-reader css; scrum cards dnd styling wip

--- apps/crm/client/src/assets/svg/icons/_IconReferences.md
+++ apps/crm/client/src/assets/svg/icons/_IconReferences.md
@@ -1,2 +1,3 @@
 https://github.com/480-Design/Solar-Icon-Set/tree/main/icons/SVG/Outline
 https://dazzleui.pro/
+https://solar-icons.vercel.app/
---
+++ apps/crm/client/src/components/svg/IconArrowRightDoubleAlt.tsx
@@ -0,0 +1,25 @@
+import type { IProps } from './';
+
+function IconArrowRightDoubleAlt({ svgClass = undefined }: IProps): React.JSX.Element {
+  return (
+    <svg className={svgClass} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <title>Icon Arrow Double Right</title>
+      <path
+        d="M11 19L17 12L11 5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.99982 19L12.9998 12L6.99982 5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default IconArrowRightDoubleAlt;
