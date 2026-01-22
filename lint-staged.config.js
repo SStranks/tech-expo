252c98faf7f7125d58022d44e3b3fe97a5f754d7
Simon Stranks <sstranks87@gmail.com>
2026-01-22 18:31:09 +0000

    chore(tech-expo): added sigint handling for husky precommit script

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,20 @@
-/* eslint-disable unicorn/filename-case */
-
 /**
  * @filename: lint-staged.config.js
  * @type {import('lint-staged').Configuration}
  */
 
 export default {
-  '*': 'secretlint',
+  '*': ['secretlint'],
+  // '*': ['secretlint', 'editorconfig-checker'],
+  // '.{css, scss, sass}': ['stylelint', 'prettier --check'],
+  // '*.sh': 'shellcheck',
+  // '*.css': ['stylelint', 'prettier --check'],
+  // 'Dockerfile*': 'hadolint',
+  // '.env*': 'dotenv-linter',
+  // '*.graphql': ['eslint', 'prettier --check'],
+  // '*.html': ['eslint', 'htmlhint', 'prettier --check'],
+  // '*.{js,ts,jsx,tsx}': ['eslint --max-warnings=0', 'prettier --check'],
+  // '*.json': ['eslint', 'prettier --check'],
+  // '*.md': ['markdownlint-cli2', 'prettier --check'],
+  // '*.{yml,yaml}': ['eslint', 'yamllint', 'prettier --check'],
 };
