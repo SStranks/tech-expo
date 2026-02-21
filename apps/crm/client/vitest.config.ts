29e86f772fd0a7f27e80b05b7f74455c4df3b8ff
Simon Stranks <sstranks87@gmail.com>
2026-02-21 19:45:33 +0000

    fix(tech-expo): vitest with github workflow

--- apps/crm/client/vitest.config.ts
+++ apps/crm/client/vitest.config.ts
@@ -10,62 +10,64 @@ import url from 'node:url';
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
-export default defineConfig({
-  plugins: [viteTsconfigPathsPlugin(), viteReactPlugin()],
-  css: {
-    preprocessorOptions: {
-      scss: {},
-    },
-  },
-  test: {
-    env: loadEnv('dev', process.cwd(), ''),
-    environment: 'jsdom',
-    globals: true,
-    include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-    setupFiles: ['./vitest.setup.ts'],
-    coverage: {
-      include: ['src/**.{js,jsx,ts,tsx}'],
-    },
-    exclude: [
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/cypress/**',
-      '**/__snapshots__/**',
-      '**/.sassdoc/**',
-      '**/.storybook/**',
-      '**/private/**',
-      '**/private*',
-      '**/public/**',
-      '**/webpack/**',
-      '**/.{idea,git,cache,output,temp}/**',
-      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-    ],
-    projects: [
-      {
-        extends: true,
-        test: {
-          name: 'unit',
-        },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [viteTsconfigPathsPlugin(), viteReactPlugin()],
+    css: {
+      preprocessorOptions: {
+        scss: {},
       },
-      {
-        plugins: [
-          storybookTest({
-            configDir: path.join(__dirname, '.storybook'),
-            storybookScript: 'pnpm run storybook',
-          }),
-          viteReactPlugin(),
-        ],
-        test: {
-          name: 'storybook',
-          setupFiles: ['./.storybook/vitest.setup.ts'],
-          browser: {
-            enabled: true,
-            headless: true,
-            instances: [{ browser: 'chromium' }],
-            provider: playwright({}),
+    },
+    test: {
+      env: loadEnv(`${mode}.client`, process.cwd(), ''),
+      environment: 'jsdom',
+      globals: true,
+      include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+      setupFiles: ['./vitest.setup.ts'],
+      coverage: {
+        include: ['src/**.{js,jsx,ts,tsx}'],
+      },
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/cypress/**',
+        '**/__snapshots__/**',
+        '**/.sassdoc/**',
+        '**/.storybook/**',
+        '**/private/**',
+        '**/private*',
+        '**/public/**',
+        '**/webpack/**',
+        '**/.{idea,git,cache,output,temp}/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      ],
+      projects: [
+        {
+          extends: true,
+          test: {
+            name: 'unit',
           },
         },
-      },
-    ],
-  },
+        {
+          plugins: [
+            storybookTest({
+              configDir: path.join(__dirname, '.storybook'),
+              storybookScript: 'pnpm run storybook',
+            }),
+            viteReactPlugin(),
+          ],
+          test: {
+            name: 'storybook',
+            setupFiles: ['./.storybook/vitest.setup.ts'],
+            browser: {
+              enabled: true,
+              headless: true,
+              instances: [{ browser: 'chromium' }],
+              provider: playwright({}),
+            },
+          },
+        },
+      ],
+    },
+  };
 });
--- apps/crm/server/vitest.config.ts
+++ apps/crm/server/vitest.config.ts
@@ -6,46 +6,48 @@ import url from 'node:url';
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '#App': path.resolve(__dirname, 'src/app'),
-      '#Config': path.resolve(__dirname, 'src/config'),
-      '#Controllers': path.resolve(__dirname, 'src/controllers'),
-      '#Data': path.resolve(__dirname, 'src/data'),
-      '#Graphql': path.resolve(__dirname, 'src/graphql'),
-      '#Helpers': path.resolve(__dirname, 'src/helpers'),
-      '#Lib': path.resolve(__dirname, 'src/lib'),
-      '#Log': path.resolve(__dirname, 'src/log'),
-      '#Mappers': path.resolve(__dirname, 'src/mappers'),
-      '#Middleware': path.resolve(__dirname, 'src/middleware'),
-      '#Models': path.resolve(__dirname, 'src/models'),
-      '#Routes': path.resolve(__dirname, 'src/routes'),
-      '#Services': path.resolve(__dirname, 'src/services'),
-      '#Tests': path.resolve(__dirname, 'src/tests'),
-      '#Types': path.resolve(__dirname, 'src/types'),
-      '#Utils': path.resolve(__dirname, 'src/utils'),
-      '#Views': path.resolve(__dirname, 'src/views'),
+export default defineConfig(({ mode }) => {
+  return {
+    resolve: {
+      alias: {
+        '#App': path.resolve(__dirname, 'src/app'),
+        '#Config': path.resolve(__dirname, 'src/config'),
+        '#Controllers': path.resolve(__dirname, 'src/controllers'),
+        '#Data': path.resolve(__dirname, 'src/data'),
+        '#Graphql': path.resolve(__dirname, 'src/graphql'),
+        '#Helpers': path.resolve(__dirname, 'src/helpers'),
+        '#Lib': path.resolve(__dirname, 'src/lib'),
+        '#Log': path.resolve(__dirname, 'src/log'),
+        '#Mappers': path.resolve(__dirname, 'src/mappers'),
+        '#Middleware': path.resolve(__dirname, 'src/middleware'),
+        '#Models': path.resolve(__dirname, 'src/models'),
+        '#Routes': path.resolve(__dirname, 'src/routes'),
+        '#Services': path.resolve(__dirname, 'src/services'),
+        '#Tests': path.resolve(__dirname, 'src/tests'),
+        '#Types': path.resolve(__dirname, 'src/types'),
+        '#Utils': path.resolve(__dirname, 'src/utils'),
+        '#Views': path.resolve(__dirname, 'src/views'),
+      },
     },
-  },
-  test: {
-    env: loadEnv('dev', process.cwd(), ''),
-    environment: 'node',
-    globals: true,
-    include: ['src/**/*.{test,spec}.?(c|m)[jt]s'],
-    setupFiles: ['./vitest.setup.ts'],
-    coverage: {
-      include: ['src/**.{js,ts}'],
+    test: {
+      env: loadEnv(`${mode}.server`, process.cwd(), ''),
+      environment: 'node',
+      globals: true,
+      include: ['src/**/*.{test,spec}.?(c|m)[jt]s'],
+      setupFiles: ['./vitest.setup.ts'],
+      coverage: {
+        include: ['src/**.{js,ts}'],
+      },
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/__snapshots__/**',
+        '**/private/**',
+        '**/private*',
+        '**/public/**',
+        '**/.{idea,git,cache,output,temp}/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      ],
     },
-    exclude: [
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/__snapshots__/**',
-      '**/private/**',
-      '**/private.*.test.*',
-      '**/public/**',
-      '**/.{idea,git,cache,output,temp}/**',
-      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-    ],
-  },
+  };
 });
