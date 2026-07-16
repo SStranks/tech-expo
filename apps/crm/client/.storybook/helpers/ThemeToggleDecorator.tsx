c3c66663d5beaa87f4e5346d67156330f2f23264
Simon Stranks <sstranks87@gmail.com>
2026-07-16 14:54:51 +0100

    fix(storybook): removed useRef in theme decorator

--- apps/crm/client/.storybook/helpers/ThemeToggleDecorator.tsx
+++ apps/crm/client/.storybook/helpers/ThemeToggleDecorator.tsx
@@ -2,7 +2,7 @@ import type { Decorator } from '@storybook/react-webpack5';
 
 import type { StorybookGlobals } from '../preview';
 
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { useGlobals } from 'storybook/internal/preview-api';
 
 import { ThemeToggle } from './ThemeToggle';
@@ -16,31 +16,23 @@ body.classList.add(styles['Storybook-ThemeToggle']);
 const ThemeToggleDecorator: Decorator = (Story, context) => {
   const [globals, updateGlobals] = useGlobals();
   const { theme, themeBg } = globals as StorybookGlobals;
-  const themeRef = useRef(theme);
-  const themeBgRef = useRef(themeBg);
 
   useEffect(() => {
     // user switches light/dark theme toggle
-    if (theme !== themeRef.current) {
-      if (theme === 'light') {
-        body.classList.remove('dark-theme');
-        body.style.setProperty('--thm-background-screen', themeBg.light);
-      }
-      if (theme === 'dark') {
-        body.classList.add('dark-theme');
-        body.style.setProperty('--thm-background-screen', themeBg.dark);
-      }
 
-      themeRef.current = theme;
-      return;
+    if (theme === 'light') {
+      body.classList.remove('dark-theme');
+      body.style.setProperty('--thm-background-screen', themeBg.light);
     }
+    if (theme === 'dark') {
+      body.classList.add('dark-theme');
+      body.style.setProperty('--thm-background-screen', themeBg.dark);
+    }
+  }, [theme, themeBg]);
 
+  useEffect(() => {
     // user adjusts light/dark theme colour value
-    if (themeBg.light !== themeBgRef.current.light) body.style.setProperty('--thm-background-screen', themeBg.light);
-    if (themeBg.dark !== themeBgRef.current.dark) body.style.setProperty('--thm-background-screen', themeBg.dark);
-
-    themeBgRef.current = themeBg;
-    return;
+    body.style.setProperty('--thm-background-screen', theme === 'dark' ? themeBg.dark : themeBg.light);
   }, [theme, themeBg]);
 
   return (
